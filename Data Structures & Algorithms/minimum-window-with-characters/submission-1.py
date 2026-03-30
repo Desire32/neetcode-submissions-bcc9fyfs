08c7aa50ec8f1f787de194dfb6e5ec7e66e81e7a
Nikita Markov <krutue@mail.ru>
2026-03-30 15:08:46 +0300

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            return ""
+        if len(t) == len(s) and t != s:
+            return ""
+        unique_chars, store = set(), dict()
+        n, window_len = len(s), len(t)
+        result = ""
+
+        # 1. Store all unique chars of t
+        for ch in t:
+            unique_chars.add(ch)
+
+        # 2. Store positions of simillar chars    
+        for i, ch in enumerate(s):
+            if ch in unique_chars:
+                store[ch] = i
+
+        L, R = min(store.values()), max(store.values()) # left and right border of sliding window
+        
+        print(L , R)
+        for i in range(L, R + 1):
+            result += s[i]
+
+        print(unique_chars)
+        print(store)
+
+        return result
